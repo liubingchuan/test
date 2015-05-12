f2161a336b6d11e31932c67d5652c3ae8b78c599
liubingchuan8@gmail.com <liubingchuan8@gmail.com>
2015-05-12 16:21:35 +0800

    test project for committing

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String s = "2.4.5.6";
+		if(s.startsWith("23.")) {
+			String[] str = s.split("\\.");
+			System.out.println(str[str.length-1]);
+		}
+	}
+}
